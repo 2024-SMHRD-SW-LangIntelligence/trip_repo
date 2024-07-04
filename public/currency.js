77d797b45130a38f6c3bc3dc5a8fb498c50b80c0
SungmoonJeong <palebluedot901@gmail.com>
2024-07-04 16:35:48 +0900

    Changes to be committed:     2024.07.04 정성문     public/currency.js 파일 환율정보 뜨도록 완료함.     유로 국가 추가하여 유로 뜨도록 수정 	modified:   public/currency.js

--- public/currency.js
+++ public/currency.js
@@ -1,6 +1,17 @@
+// 유로존 국가 리스트
+const euroZoneCountries = [
+  '오스트리아', '벨기에', '핀란드', '프랑스',
+  '독일', '아일랜드', '이탈리아', '룩셈부르크',
+  '네덜란드', '포르투갈', '스페인',
+  '그리스', '슬로베니아',
+  '키프로스', '몰타',
+  '슬로바키아', '에스토니아',
+  '라트비아', '리투아니아', '크로아티아'
+];
+
 // Fetch currency data by country name
 function fetchCurrencyDataByCountry(countryName) {
-  fetch('/currencies')
+  fetch('http://localhost:3000/currencies')
     .then(response => {
       if (!response.ok) {
         throw new Error('네트워크 상태가 좋지 않습니다.');
@@ -9,18 +20,22 @@ function fetchCurrencyDataByCountry(countryName) {
     })
     .then(data => {
       console.log('서버로부터 받은 데이터:', data);
-      const countryData = data.filter(item => {
-        console.log('비교:', item[2].trim().toLowerCase(), countryName.trim().toLowerCase());
-        return item[2].trim().toLowerCase() === countryName.trim().toLowerCase();
-      }); // 선택된 국가의 데이터
-      displayCurrencies(countryData);
+
+      // 유로존 국가 리스트에 포함된 경우
+      if (euroZoneCountries.includes(countryName.trim())) {
+        const euroData = data.filter(item => item[2].trim().toLowerCase() === '유로');
+        displayCurrencies(euroData);
+      } else { // 그 외 국가인 경우
+        const countryData = data.filter(item => item[2].trim().toLowerCase() === countryName.trim().toLowerCase());
+        displayCurrencies(countryData);
+      }
     })
     .catch(error => {
       console.error('데이터를 가져오는 중에 오류가 발생했습니다:', error);
     });
 }
 
-// 사이드바에 통화 데이터를 표시하는 함수
+// 국가의 통화 데이터를 표시하는 함수
 function displayCurrencies(data) {
   const sidebarContent = document.getElementById('sidebarContent');
   sidebarContent.innerHTML = ''; // 기존 내용을 초기화
@@ -36,12 +51,6 @@ function displayCurrencies(data) {
     const countryName = item[2]; // 국가 이름
     const countryCode = item[3]; // 국가 코드
 
-    // 유효성 검사
-    if (!currencyCode || !currencyValue || !countryName || !countryCode) {
-      console.warn(`Invalid data for country ${countryName}: [${currencyCode}, ${currencyValue}, ${countryCode}]`);
-      return;
-    }
-
     // 데이터 표시
     const currencyItem = document.createElement('div');
     currencyItem.className = 'currency-item';
@@ -49,8 +58,8 @@ function displayCurrencies(data) {
       <h3>${countryName}</h3>
       <p>Currency Code: ${currencyCode}</p>
       <p>Value: ${currencyValue}</p>
-      <p>Country Code: ${countryCode}</p>
     `;
     sidebarContent.appendChild(currencyItem);
   });
 }
+
