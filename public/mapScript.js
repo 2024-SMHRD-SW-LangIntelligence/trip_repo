35b5a9b7afad85ccaa314786f952c465224f4221
임채환 <pfcv123@gmail.com>
2024-07-10 14:45:53 +0900

    07/10 14:46 임채환 사이드바 디테일 적용  Changes to be committed: 	modified:   node_modules/.package-lock.json 	modified:   package-lock.json 	modified:   public/Main.html 	modified:   public/mapScript.js 	modified:   public/styles.css

--- public/mapScript.js
+++ public/mapScript.js
@@ -165,7 +165,7 @@ function initializeMap() {
 // Google Maps API 스크립트 로드
 function loadGoogleMapsScript() {
   const script = document.createElement('script');
-  script.src = 'https://maps.googleapis.com/maps/api/js?key=Your_Key&callback=initializeMap';
+  script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDAceqhgqco1E6Dw29YqfEY_hjDB4SXEwQ&callback=initializeMap';
   script.defer = true;
   script.async = true;
   document.head.appendChild(script);
